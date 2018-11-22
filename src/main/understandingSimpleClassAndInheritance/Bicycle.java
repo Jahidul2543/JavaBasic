30eb63d4366a1019676b1e53e9be13788abcfe01
Md Jahidul Islam <jahidul@Mds-MacBook-Pro.local>
2018-11-21 23:23:39 -0500

    changed Bicycle.java class

--- src/main/understandingSimpleClassAndInheritance/Bicycle.java
+++ src/main/understandingSimpleClassAndInheritance/Bicycle.java
@@ -1,21 +1,32 @@
 package main.understandingSimpleClassAndInheritance;
 
+
+
 class Bicycle {
 
     int cadence = 0;
     int speed = 0;
     int gear = 1;
+     String cycleBrand = "Bmx";
+     char grade = 'A';
+     double number= 4.000000000;
+     float floatNumber= 4.00000000F;
+     long longDataType = 12000000000L;
+     boolean lightIsOn = false;
 
     void changeCadence(int newValue) {
         cadence = newValue;
+        System.out.println("This will change cadence");
     }
 
     void changeGear(int newValue) {
         gear = newValue;
+        System.out.println(" This will change gear");
     }
 
     void speedUp(int increment) {
         speed = speed + increment;
+        System.out.println("This will speedup");
     }
 
     void applyBrakes(int decrement) {
@@ -29,4 +40,18 @@ class Bicycle {
                         " speed:" + speed+
                         " gear:" + gear);
     }
+
+
+
+    public static void main(String[] args) {
+        // We create object of Bicycle type
+        Bicycle jenysBicycle = new Bicycle();
+
+        jenysBicycle.changeCadence(20);
+        jenysBicycle.changeGear(4);
+        jenysBicycle.speedUp(15);
+
+
+    }
+
 }
