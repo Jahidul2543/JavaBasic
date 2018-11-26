239f7fe83e4e670bffd7863444c432c5af9740a2
Jahidul2543 <34196568+Jahidul2543@users.noreply.github.com>
2018-11-26 17:46:43 -0500

    Update EnumTest.java

--- src/main/EnumTest.java
+++ src/main/EnumTest.java
@@ -18,7 +18,7 @@ public class EnumTest {
                 System.out.println("Happy day coming");
                 break;
             case THU:
-                System.out.println("Working from home day");
+                System.out.println("Working from home");
             case FRI:
                 System.out.println("Waiting for weekend");
                 break;
@@ -28,6 +28,21 @@ public class EnumTest {
     public static void main(String[] args) {
      EnumTest objOfEnumTest = new EnumTest(Day.FRI);
      objOfEnumTest.howWillBeTheDay();
+     EnumTest objOfEnumTest2 = new EnumTest(Day.MON);
+     objOfEnumTest2.howWillBeTheDay();
 
-    }
+     for (Day d : Day.values()){
+
+         System.out.println( "Print all enums from Day " + d);
+      }
+    } 
 }
+
+/*
+* 
+* All enums implicitly extend java.lang.Enum. Because a class can
+* only extend one parent (see Declaring Classes), the Java language
+* does not support multiple inheritance of state (see Multiple Inheritance of State,
+* Implementation, and Type), and therefore an enum cannot extend anything else.
+* 
+* */
